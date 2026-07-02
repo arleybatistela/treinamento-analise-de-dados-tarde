ab436c06902ac23f84f76d560b7006cf52ea7187
emiyu924 <emillyyumi124@gmail.com>
2026-07-02 16:41:11 -0300

    feat: adiciona notebook da aulda de análise de dados3

---
+++ aula01.py
@@ -0,0 +1,20 @@
+#O coando print, exibe uma informação na tela do usuário.
+print("Olá Mundo!")
+print("Iniciando treinamnto de análise de Dados")
+print(30) #idade do usuário
+print("30") #idade do usuário
+
+idade = 30
+idade_dois = "30"
+
+print(idade)
+print(idade_dois)
+
+idade_futura = idade + 1
+print(idade_futura)
+
+idade_futura = idade + 1
+print(idade_futura)
+
+print(type(idade))
+print(type(idade_dois))
